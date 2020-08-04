deb3ddffffc0db132dec7e9cbf27d6e696bfd45d
chitang233 <bilibilichitang@hotmail.com>
2020-08-04 16:11:55 +0800

    Add Easy Number-Guess game

---
+++ Guess-number/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int guess,number;
+	number = 4;
+	cout << "Guess a number :";
+	cin >> guess;
+	if (guess == 4)
+	{
+		cout << "Oh Yeah!You're right!";
+	}
+	else if (guess < 4)
+	{
+		cout << "Guess number is too SMALL";
+	}
+	else if (guess > 4)
+	{
+		cout << "Guess number is too BIG";
+	}
+	return 0;
+}
