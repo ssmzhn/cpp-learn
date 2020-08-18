99b5c64d6715f4f520d0c94a07cec674b97b8711
David Zhang <kiteabpl@outlook.com>
2020-08-19 00:33:28 +0800

    fix code style make it look cooler than before

--- Guess-number/main.cpp
+++ Guess-number/main.cpp
@@ -1,16 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
 	int guess;
-	cout << "Guess a number :";
-	cin >> guess;
-	if (guess > 4)
-		cout << "Guess number is too BIG";
-	else if (guess < 4)
-		cout << "Guess number is too SMALL";
-	else
-		cout << "Oh Yeah!You're right!";
+	
+	while (true)
+	{
+		cout << "Guess a number: ";
+		cin >> guess;
+		if (guess > 4)
+		{
+			cout << "Guess number is too BIG" << endl;
+		} else if (guess < 4) {
+			cout << "Guess number is too SMALL" << endl;
+		} else {
+			cout << "Oh Yeah! You're right!" << endl;
+			break;
+		}
+	}
 	return 0;
 }
