cab615fb7dc8bb079a7b2aa839388592bd586aa5
David Zhang <kiteabpl@outlook.com>
2020-08-19 00:10:57 +0800

    the comments and details  woo-hoo~

--- Repeater/main.cpp
+++ Repeater/main.cpp
@@ -1,11 +1,23 @@
-#include<iostream>
-using namespace std;
+#include <iostream> // cout and cin needs
+using namespace std;// dont need std::cout again~
 
-int main(void)
+int main()
 {
-	string text;
-	cout << "Input something pls : ";
-	cin >> text;
-	cout << endl << text;
+	string txt; // a string type variable for save user type things
+	
+	// the main loop
+	while (true)
+	{
+		// get text
+		cout << "Input something pls (q for exit): ";
+		cin >> txt;
+		// if user want exit the prog, then do it
+		if (txt == "q")
+		{
+			cout << "See you later" << endl;
+			exit(0); // 0 for normal exit code
+		}
+		cout << endl << txt; // if not the "q", do a repeater and jump in the next loop
+	}
 	return 0;
 }
