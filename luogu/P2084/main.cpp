6be09729e131c2e992b8b90dafd22cda73b3ae58
Chi_Tang <bilibilichitang@hotmail.com>
2021-10-10 14:52:46 +0800

    Fuck

---
+++ luogu/P2084/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+  int m, n; // 声明 m n
+  std::cin >> m >> n; // 输入 m n
+  std::string out;  // 声明输出用 string
+  std::string strN = std::to_string(n); // 创建一个与 n 值相同的 String
+  int nLength = strN.size();  // 获取 n 的长度
+  switch (m) // 1*2^4+0*2^3+1*2^2+0*2^1+1*2^0，
+  {
+  case 1:
+    
+    break;
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  }
+  return 0;
+}
