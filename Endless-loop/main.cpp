f17ff40d8edc43a41d7692ec27cbce894ab77ac6
SSMZHN <ssmzhn@163.com>
2022-01-25 21:30:11 +0800

    Shorter

--- Endless-loop/main.cpp
+++ Endless-loop/main.cpp
@@ -3,6 +3,6 @@ int main()
 {
 	int i=1; //int a number
 	for (;;i++) //make i = 1 before loop
-		printf("%d",i); //print i's vaule on screen
+		printf("%d\n",i); //print i's vaule on screen
 	return 0; //exit the program normal
 }
